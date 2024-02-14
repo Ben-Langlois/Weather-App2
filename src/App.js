1e5f968ea9ec344c3689ad61647ff7577c3e9127
Ben Langlois <benlangloiss@gmail.com>
2024-02-14 17:07:25 -0500

    trying to get placeSelect to work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 // import logo from './logo.svg';
-import { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import './App.scss';
-import React from 'react';
+// import React from 'react';
 // import $ from 'jquery';
 // import * as icons from './icons/icons.js';
 import { GeoapifyGeocoderAutocomplete, GeoapifyContext } from '@geoapify/react-geocoder-autocomplete'
@@ -12,54 +11,37 @@ var autocompleteKey = '62e93b34c2ee4337b92e9b81d777029a';
 
 
 
-function WeatherApp2() {
-  const [city, setCity] = useState('')
+const WeatherApp2 = () => {
+  const [city, setCity] = useState('');
   
-
-  // const autocomplete = new GeocoderAutocomplete(
-  //   document.getElementById("autocomplete"), 
-  //   autocompleteKey, 
-  //   { /* Geocoder options */ });
-
+  // componentDidMount (essentially)
   useEffect (() => {
-    // const autocomplete = new GeocoderAutocomplete(
-    //   document.getElementById("autocomplete"), 
-    //   autocompleteKey, 
-    //   { /* Geocoder options */ });
+    console.log('Mounted?')
+  }, []);
 
-    // When location is selected
-    // autocomplete.on('select', (location) => {
-    //   // console.log(location);
-    //   // this.setState({
-    //   //   city: location.properties.city,
-    //   //   formatted: location.properties.formatted,
-    //   //   country: location.properties.country,
-    //   //   lat: location.properties.lat,
-    //   //   lon: location.properties.lon
-    //   // });
-    //   // console.log(this.state)
-
-    //   // this.handleSubmit();                                          
-
-    // });
-  })
+  const onPlaceSelect = (value) => {
+    console.log(value);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
           <GeoapifyContext id='input-container' apiKey={autocompleteKey}>
-            <GeoapifyGeocoderAutocomplete id='input' placeholder="Enter address here"
+            <GeoapifyGeocoderAutocomplete placeholder="Enter address here"
               // type={type}
               // lang={language}
               // position={position}
               // countryCodes={countryCodes}
               // limit={limit}
               // value={displayValue}
-              // placeSelect={onPlaceSelect}
+              placeSelect={onPlaceSelect}
               // suggestionsChange={onSuggectionChange}
-              />
+            />
           </GeoapifyContext>          
       </header>
+      <div id='dashboard'>
+
+      </div>
     </div>
   );
 }
